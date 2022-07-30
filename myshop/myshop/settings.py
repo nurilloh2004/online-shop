cb6aac853efdc1741e77e5771c1777a17a1ea63b
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2022-07-30 15:59:51 +0500

    add brain tree

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -131,15 +131,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Braintree settings
-# BRAINTREE_MERCHANT_ID = ''  # Merchant ID
-# BRAINTREE_PUBLIC_KEY = ''   # Public Key
-# BRAINTREE_PRIVATE_KEY = ''  # Private key
+BRAINTREE_MERCHANT_ID = ''  # Merchant ID
+BRAINTREE_PUBLIC_KEY = ''   # Public Key
+BRAINTREE_PRIVATE_KEY = ''  # Private key
 
-# import braintree
+import braintree
 
-# BRAINTREE_CONF = braintree.Configuration(
-#     braintree.Environment.Sandbox,
-#     BRAINTREE_MERCHANT_ID,
-#     BRAINTREE_PUBLIC_KEY,
-#     BRAINTREE_PRIVATE_KEY
-# )
+BRAINTREE_CONF = braintree.Configuration(
+    braintree.Environment.Sandbox,
+    BRAINTREE_MERCHANT_ID,
+    BRAINTREE_PUBLIC_KEY,
+    BRAINTREE_PRIVATE_KEY
+)
