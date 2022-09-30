2de377308fbce45df67db89f14f76b5bc0834697
nurilloh2004 <nurillohpython@gmail.com>
2022-09-30 18:09:17 +0500

    *

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -99,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
 ]
 
 
